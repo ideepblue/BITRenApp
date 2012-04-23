9343b280df3dc171a82501f5501cc24bea0c33a8
ideepblue <deepblue.aesky@gmail.com>
2012-04-23 18:52:11 +0800

    增加数据更新和软件更新的功能

---
+++ src/org/bitren/app/BitrenActions.java
@@ -0,0 +1,5 @@
+package org.bitren.app;
+
+public class BitrenActions {
+	public static final String ACTION_REFRESH_CONTACT = "ACTION_REFRESH_CONTACT";
+}
---
+++ src/org/bitren/app/database/TableTimestampColumns.java
@@ -0,0 +1,10 @@
+package org.bitren.app.database;
+
+import android.provider.BaseColumns;
+
+public class TableTimestampColumns implements BaseColumns {
+	public static final String TABLE_NAME = "table_timestamp";
+	
+	public static final String NAME = "name";
+	public static final String TIMESTAMP = "timestamp";
+}
